6e6e1c596bc2aa2bc0d95002480696eab43c261d
Andrii-Bondarenko <bondarenko_andrii@outlook.com>
2018-10-04 23:42:41 +0300

    https

--- public/index.php
+++ public/index.php
@@ -4,8 +4,14 @@ use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
+if (strpos($_SERVER['REQUEST_URI'],'/public') === 0) {
+    header("HTTP/1.0 404 Not Found");
+    header("Location: https://part-store.com.ua/notFound");
+    exit;
+}
 
 require __DIR__.'/../vendor/autoload.php';
+
 
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV'])) {
